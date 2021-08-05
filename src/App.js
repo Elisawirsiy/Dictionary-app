8bcabea52d0a3e7f8a12613ac64345c8f3c4f27c
Elizabeth Wirsiy <elisawirsiy@gmail.com>
2021-08-06 02:09:10 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dictionary App</h1>
+
+        <p>Coming soon...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
